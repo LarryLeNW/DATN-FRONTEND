bcd1abc20d1665616ffd92ef8efca9ef4d30561d
nguyen van quyet <nguyenquyet2017zz@gmail.com>
2024-11-27 12:33:50 +0700

    nvq hanlde comment and reply blog

--- src/pages/public/BlogDetail/index.jsx
+++ src/pages/public/BlogDetail/index.jsx
@@ -3,6 +3,7 @@ import DOMPurify from "dompurify";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import CommentBlog from "../CommentBlog";
 
 const BlogDetail = () => {
     const { blogId } = useParams();
@@ -70,38 +71,7 @@ const BlogDetail = () => {
                         </article>
                     )}
                     <section className="mt-10">
-                        <h3 className="text-2xl font-bold text-gray-800 mb-6">
-                            Bài viết liên quan
-                        </h3>
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                            <div className="bg-white p-4 rounded-lg shadow">
-                                <h4 className="text-xl font-semibold text-gray-800 mb-2">
-                                    Tiêu đề liên quan 1
-                                </h4>
-                                <p className="text-gray-500 text-sm">
-                                    Nội dung tóm tắt ngắn gọn cho bài viết liên
-                                    quan.
-                                </p>
-                            </div>
-                            <div className="bg-white p-4 rounded-lg shadow">
-                                <h4 className="text-xl font-semibold text-gray-800 mb-2">
-                                    Tiêu đề liên quan 2
-                                </h4>
-                                <p className="text-gray-500 text-sm">
-                                    Nội dung tóm tắt ngắn gọn cho bài viết liên
-                                    quan.
-                                </p>
-                            </div>
-                            <div className="bg-white p-4 rounded-lg shadow">
-                                <h4 className="text-xl font-semibold text-gray-800 mb-2">
-                                    Tiêu đề liên quan 3
-                                </h4>
-                                <p className="text-gray-500 text-sm">
-                                    Nội dung tóm tắt ngắn gọn cho bài viết liên
-                                    quan.
-                                </p>
-                            </div>
-                        </div>
+                    <CommentBlog></CommentBlog>
                     </section>
                 </main>
 
